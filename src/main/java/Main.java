96f556cfb0328ef078b5407e43027675045908b9
Leo24dotcom <leo491131@gmail.com>
2025-01-24 10:35:55 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,18 @@ public static void main(final String[] args) {
 String[] lines = {"beast","dough","happy","question","star","three","eagle","try"};
 public void setup(){
   for(int i = 0; i < lines.length;i++){
-    if(findFirstVowel(lines[i]) == -1)
+    if(findFirstVowel(lines[i]) == -1){
      System.out.println(lines[i] + "ay");
-    else if(findFirstVowel(lines[i]) == 0)
+    }
+    else if(findFirstVowel(lines[i]) == 0){
      System.out.println(lines[i] + "way");
-    else if(lines[i].substring(0,2).equals("qu"))
+    }
+    else if(lines[i].substring(0,2).equals("qu")){
       System.out.println(lines[i].substring(2,lines[i].length()) + "qu" + "ay");
-     else
+    }
+     else{
    System.out.println(lines[i].substring(findFirstVowel(lines[i]),lines[i].length()) + lines[i].substring(0,findFirstVowel(lines[i])) +"ay");
+     }
   }
 }
 	
